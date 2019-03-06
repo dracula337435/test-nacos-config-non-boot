5e11df04e535e50ed50dc6f8a60dcec9df67f88e
dk <dracula337435@163.com>
2019-03-06 20:03:21 +0800

    若server-addr使用占位符，xml方式不解析，应该用@EnableNacosConfig。跟了一下源码，xml方式也有resolve动作，但是PropertySource中没有配置文件；注解方式则是有。

--- src/main/java/io/dracula/test/nacos/non/boot/MyConfig.java
+++ src/main/java/io/dracula/test/nacos/non/boot/MyConfig.java
@@ -1,5 +1,6 @@
 package io.dracula.test.nacos.non.boot;
 
+import com.alibaba.nacos.spring.context.annotation.config.EnableNacosConfig;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.PropertySource;
 @ComponentScan
 @Configuration
 @PropertySource("classpath:io/dracula/test/nacos/non/boot/main.properties")
+@EnableNacosConfig
 public class MyConfig{
 
 }
