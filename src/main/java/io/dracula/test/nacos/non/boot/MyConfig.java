bb63314fd25feb914cd9e219388683a2c90463f5
dk <dracula337435@163.com>
2019-03-06 15:38:15 +0800

    地址可用占位符

--- src/main/java/io/dracula/test/nacos/non/boot/MyConfig.java
+++ src/main/java/io/dracula/test/nacos/non/boot/MyConfig.java
@@ -1,6 +1,5 @@
 package io.dracula.test.nacos.non.boot;
 
-import com.alibaba.nacos.api.annotation.NacosProperties;
 import com.alibaba.nacos.spring.context.annotation.config.EnableNacosConfig;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +11,7 @@ import org.springframework.context.annotation.PropertySource;
 @ComponentScan
 @Configuration
 @PropertySource("classpath:io/dracula/test/nacos/non/boot/main.properties")
-@EnableNacosConfig(globalProperties = @NacosProperties(serverAddr = "127.0.0.1:8848"))
-public class MyConfig {
+@EnableNacosConfig
+public class MyConfig{
+
 }
